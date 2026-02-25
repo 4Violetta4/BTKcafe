fbc1cfcd32e87c4a213f85555f26afc78df9a4b6
Violetta <violettmer@gmail.com>
2026-02-25 13:40:55 +0300

    feat: add page 404 and photo webp

--- assests/js/script.js
+++ assests/js/script.js
@@ -10,32 +10,33 @@
 
     function renderCatalog(data) {
         const catalog = document.getElementById('catalog');
+        catalog.classList.add('container');
 
         data.forEach(category => {
             const section = document.createElement('section');
 
             const container = document.createElement('div');
-            container.classList.add('container');
+            container.classList.add('container__card');
 
             const title = document.createElement('h2');
             title.textContent = category.name;
 
-            container.appendChild(title);
+            section.appendChild(title);
 
             category.list.forEach(item => {
                 const card = document.createElement('div');
-                card.classList.add('category');
+                card.classList.add('card');
 
-                const imgCategory = document.createElement('img');
-                imgCategory.classList.add('imgCategory');
-                imgCategory.src = item.image;
+                const imgCard = document.createElement('img');
+                imgCard.classList.add('imgCard');
+                imgCard.src = item.image;
 
-                const categoryText = document.createElement('div');
-                categoryText.classList.add('category_text');
+                const cardText = document.createElement('div');
+                cardText.classList.add('Card_text');
 
-                const nameVariant = document.createElement('h3');
+                const nameVariant = document.createElement('h4');
                 nameVariant.textContent = item.name;
-                categoryText.appendChild(nameVariant);
+                cardText.appendChild(nameVariant);
 
                 if (item.variant) {
 
@@ -45,7 +46,7 @@
                         p.textContent =
                             `${variant.size} ${variant.unit} — ${variant.price} ${variant.currency}`;
 
-                        categoryText.appendChild(p);
+                        cardText.appendChild(p);
                     });
 
                 } else {
@@ -54,11 +55,11 @@
                     p.textContent =
                         `${item.price} ${item.currency}`;
 
-                    categoryText.appendChild(p);
+                    cardText.appendChild(p);
                 }
 
-                card.appendChild(imgCategory);
-                card.appendChild(categoryText);
+                card.appendChild(imgCard);
+                card.appendChild(cardText);
 
                 container.appendChild(card);
             });
@@ -67,4 +68,13 @@
 
         })
     }
+
+    window.addEventListener('load', () => {
+        const loader = document.getElementById('loader');
+        loader.classList.add('hidden');
+
+        loader.addEventListener('transitionend', () => {
+            loader.remove();
+        });
+    });
 })();
