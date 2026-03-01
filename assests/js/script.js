87f09083e09173bbdee1e6bc76f84226fd3e7886
Violetta <violettmer@gmail.com>
2026-03-01 11:50:21 +0300

    feat: add rating and sort

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,10 +1,13 @@
 (function () { //важно!! Чтобы не могли обратиться
     `use strict`
+    let currentSort = "alphabet";
+
     fetch('https://api.jsonsilo.com/public/5c002cb3-8a58-43be-9c97-e1922f452414')
         .then(response => response.json())
         .then(data => {
             renderCatalog(data);
-        })
+            applyCurrentSort();
+         })
         .catch(error => console.error('Ошибка:', error));
 
 
@@ -42,6 +45,11 @@
                 const card = document.createElement('div');
                 card.classList.add('card');
 
+                const savedRating = localStorage.getItem(item.name);
+                if (savedRating) {
+                    item.rating = parseInt(savedRating);
+                }
+
                 const picture = document.createElement('picture');
 
                 const sourceWebp = document.createElement('source');
@@ -64,7 +72,7 @@
                 const cardText = document.createElement('div');
                 cardText.classList.add('Card_text');
 
-                const nameVariant = document.createElement('h4');
+                const nameVariant = document.createElement('p');
                 nameVariant.textContent = item.name;
                 cardText.appendChild(nameVariant);
 
@@ -90,31 +98,95 @@
 
                 const ratingDiv = document.createElement('div');
                 ratingDiv.classList.add('rating');
-
+                ratingDiv.dataset.rating = item.rating || 0;
                 ratingDiv.innerHTML = renderStars(item.rating || 0);
-                cardText.appendChild(ratingDiv);
 
                 ratingDiv.addEventListener('click', (e) => {
                     if (e.target.classList.contains('star')) {
-                        item.rating = parseInt(e.target.dataset.value);
-                        ratingDiv.innerHTML = renderStars(item.rating);
+
+                        const value = parseInt(e.target.dataset.value);
+
+                        item.rating = value;
+                        ratingDiv.dataset.rating = value;
+                        localStorage.setItem(item.name, value);
+
+                        ratingDiv.innerHTML = renderStars(value);
+
+                        applyCurrentSort();
                     }
                 });
+                cardText.appendChild(ratingDiv);
 
-                card.appendChild(imgCard);
+                card.appendChild(picture);
                 card.appendChild(cardText);
-
                 container.appendChild(card);
             });
             section.appendChild(container);
             catalog.appendChild(section);
+            checkId();
 
         })
+        data.forEach(category => {
+            const section = document.getElementById(category.id);
+            sortSection(section, currentSort);
+        });
     }
+
+    function checkId(){
+        setTimeout(()=>{
+            if(window.location.hash){
+                const id = window.location.hash.substring(1);
+                const element = document.getElementById(id);
+                if(element){
+                element.scrollIntoView({behavior: 'smooth'});
+                }console.log('Элемент не найден:', id);
+            }
+        },300);
+    }
+
+    function sortSection(section, type){
+        const container = section.querySelector('.container__card');
+        const cards = Array.from(container.querySelectorAll('.card'));
+
+        cards.sort((a,b)=>{
+            if(type === "alphabet") {
+                const name1 = a.querySelector('.Card_text p').textContent.toLowerCase();
+                const name2 = b.querySelector('.Card_text p').textContent.toLowerCase();
+                return name1.localeCompare(name2);
+            }
+            if(type === "rating"){
+                const rating1 = parseInt(a.querySelector('.rating').dataset.rating) || 0;
+                const rating2 = parseInt(b.querySelector('.rating').dataset.rating) || 0;
+
+                if(rating2 !== rating1) return rating2 - rating1;
+
+                const name1 = a.querySelector('.Card_text p').textContent.toLowerCase();
+                const name2 = b.querySelector('.Card_text p').textContent.toLowerCase();
+                return name1.localeCompare(name2);
+            }
+        })
+        cards.forEach(card => container.appendChild(card));
+    }
+
+    function applyCurrentSort() {
+        const sections = document.querySelectorAll('section');
+        sections.forEach(section => sortSection(section, currentSort));
+    }
+
+    window.addEventListener('DOMContentLoaded', () => {
+        const sortSelect = document.getElementById("sortSelect");
+        if (sortSelect) {
+            sortSelect.addEventListener("change", function () {
+                currentSort = this.value;
+                applyCurrentSort();
+            });
+        }
+    });
 
     window.addEventListener('load', () => {
         const loader = document.getElementById('loader');
         loader.classList.add('hidden');
+
 
         loader.addEventListener('transitionend', () => {
             loader.remove();
